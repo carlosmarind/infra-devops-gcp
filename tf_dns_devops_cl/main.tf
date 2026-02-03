5c89e629f102a097ba90b5b4184ea72dfd58f83d
Carlos Marin Diaz <carlosmarind@gmail.com>
2026-02-03 14:19:20 -0300

    se mueve cuenta de servicio de kubernetes al declaracion de dns para certmanager

--- tf_dns_devops_cl/main.tf
+++ tf_dns_devops_cl/main.tf
@@ -4,3 +4,25 @@ resource "google_dns_managed_zone" "devops_cl" {
   description   = "devops.cl Public DNS zone"
   force_destroy = "false"
 }
+
+resource "google_service_account" "sa-dns-manager" {
+  account_id                   = "sa-dns-manager"
+  display_name                 = "Service Account certmanager Kubernetes lab"
+  create_ignore_already_exists = true
+}
+
+resource "google_project_iam_member" "sa-dns-manager-iam-member" {
+  project = var.project
+  role    = "roles/dns.admin"
+  member  = "serviceAccount:${google_service_account.sa-dns-manager.email}"
+}
+
+
+resource "google_service_account_key" "key" {
+  service_account_id = google_service_account.sa-dns-manager.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+}
+resource "local_file" "service_account" {
+  content  = base64decode(google_service_account_key.key.private_key)
+  filename = "${path.module}/output/sa-dns-manager.json"
+}
